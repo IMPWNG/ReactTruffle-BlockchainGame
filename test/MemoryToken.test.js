6d6a9c5fdee9626e55c47e3d2a1babfabdd1bb3d
IMPWONG <matisse.acheen.pro@gmail.com>
2021-02-18 09:01:32 -0500

    Next step - The owener can see all the token

--- test/MemoryToken.test.js
+++ test/MemoryToken.test.js
@@ -30,4 +30,27 @@ contract('Memory Token', (accounts) => {
       assert.equal(symbol, 'MEMORY')
     })
   })
+  describe('token distribution', async () => {
+    let result
+
+    it('mints token', async () => {
+      await token.mint(accounts[0], 'https://www.token-uri.con/nft')
+      
+      //It should increase the total supply 
+      result = await token.totalSupply()
+      assert.equal(result.toString(), '1', 'total supply is correct ')
+
+      //It should increments the owner balance 
+      result = await token.balanceOf(accounts[0])
+      assert.equal(result.toString(), '1', 'balanceOf is correct')
+
+      //The token should belong to the owner
+      result = await token.ownerOf('1')
+      assert.equal(result.toString(), accounts[0].toString(), 'ownerOf is correct')
+      result = await token.tokenOfOwnerByIndex(accounts[0], 0)
+
+      //The owner can see the token
+    })
+  })
 })
+ 
